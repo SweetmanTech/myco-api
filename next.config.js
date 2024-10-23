8fe303b4b608e3ca5cfd34b6f5b750f682c4a819
aidenwong812 <wongaiden812@gmail.com>
2024-10-24 03:38:06 +0800

    fix CORS error

--- next.config.js
+++ next.config.js
@@ -35,6 +35,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
